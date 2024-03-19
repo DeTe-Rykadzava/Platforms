e65dcd2f6c30bfc81e85467c687c865728ac2ec4
DeTe-Rykadzava <di3378780@gmail.com>
2024-03-19 11:38:06 +0400

    upd 19.03.2024

--- Assets/Scripts/DamageObjects/Bomb.cs
+++ Assets/Scripts/DamageObjects/Bomb.cs
@@ -1,27 +1,28 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using Interfaces;
 using UnityEngine;
 
-public class Bomb : MonoBehaviour
+namespace DamageObjects
 {
-    [SerializeField] private ParticleSystem explosionEffect;
-    [SerializeField] private Transform mesh;
-    [SerializeField] private AudioSource sound;
+    public class Bomb : MonoBehaviour
+    {
+        [SerializeField] private ParticleSystem explosionEffect;
+        [SerializeField] private Transform mesh;
+        [SerializeField] private AudioSource sound;
     
-    private void OnTriggerEnter(Collider other)
-    {
-        if (!other.gameObject.TryGetComponent(out IDamageable obj)) return;
-        Destroy(mesh.gameObject);
-        StartCoroutine(PlayEffect());
-    }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.gameObject.TryGetComponent(out IDamageable obj)) return;
+            Destroy(mesh.gameObject);
+            StartCoroutine(PlayEffect());
+        }
 
-    private IEnumerator PlayEffect()
-    {
-        explosionEffect.Play();
-        sound.Play();
-        yield return new WaitForSeconds(1f);
-        Destroy(gameObject);
+        private IEnumerator PlayEffect()
+        {
+            explosionEffect.Play();
+            sound.Play();
+            yield return new WaitForSeconds(1f);
+            Destroy(gameObject);
+        }
     }
 }
